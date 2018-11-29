493455951492622f2f61568d15d90a957665d09f
broknfist <41538231+broknfist@users.noreply.github.com>
2018-11-29 22:33:19 +0100

    átló is kész
    diagonal ready, lehet csinálni, hogy a gép lépjen

--- Gamefield.cs
+++ Gamefield.cs
@@ -54,11 +54,9 @@ namespace gomoku
 				for (int j = 0; j < oszlopSzam; j++) {				
 					Button gridButton=new Button();
 					palya[i,j]=0;
-					gridButton.Content="";	
-					//gridButton.Content=palya[i,j].ToString();					
+					gridButton.Content="";				
 					gridButton.Width=40;
 					gridButton.Height=40;
-					//gridButton.Name="S"+sorSzam+"O"+oszlopSzam;
 					gridButton.Click+=buttonClick;
 					buttongrid.Children.Add(gridButton);
 					Grid.SetRow(gridButton,i);
@@ -71,32 +69,22 @@ namespace gomoku
 			Button gridButton=(Button)sender;
 			if (x_or_not) {
 				gridButton.IsEnabled=false;
-				//string[] temp1=gridButton.Name.Split('S');
-				//string[] temp2=temp1[1].Split('O');
-				//int sora_int=Convert.ToInt32(temp2[0]);
-				//int oszlopa_int=Convert.ToInt32(temp2[1]);
 				palya[Grid.GetRow(gridButton),Grid.GetColumn(gridButton)]=1;
-				//gridButton.Content=Convert.ToString(palya[sora_int,oszlopa_int]);
 				gridButton.Content="X";
-				//gridButton.Content=palya[Grid.GetRow(gridButton),Grid.GetColumn(gridButton)];
-				//gridButton.Content=Grid.GetRow(gridButton)+","+Grid.GetColumn(gridButton);
 				x_or_not=false;
 				turn_count++;
 			}else{
 				gridButton.IsEnabled=false;
 				gridButton.Content="O";
 				palya[Grid.GetRow(gridButton),Grid.GetColumn(gridButton)]=2;
-				//gridButton.Content=Grid.GetRow(gridButton)+","+Grid.GetColumn(gridButton);
-				//gridButton.Content=palya[Grid.GetRow(gridButton),Grid.GetColumn(gridButton)];
 				x_or_not=true;
 				turn_count++;
-			}
-			
+			}			
 			checkForWinner();
+			//implement 
 		}
 		private void checkForWinner(){
-			bool there_is_a_winner=false;
-			
+			bool there_is_a_winner=false;	
 			//**************sorok vizsgálta, hogy van -e nyertes?*****************
 			int count_horizontal_x=0,count_horizontal_o=0;
 			for (int i = 0; i < sorSzam; i++) {
@@ -123,11 +111,9 @@ namespace gomoku
 				}
 				count_horizontal_x=0;
 				count_horizontal_o=0;
-			}
-			
-			//**************oszlopok vizsgálta, hogy van -e nyertes?*****************
-			
-			int count_vertical_x=0,count_verticalal_o=0;
+			}			
+			//**************oszlopok vizsgálta, hogy van -e nyertes?*****************			
+			int count_vertical_x=0,count_vertical_o=0;
 			for (int i = 0; i < sorSzam; i++) {
 				for (int j = 0; j < oszlopSzam; j++) {
 					if (palya[j,i]==1) {			//csak szimmetrikus táblára jó!!!!!
@@ -139,74 +125,91 @@ namespace gomoku
 					} else {
 						count_vertical_x=0;
 					}
-					if (palya[i,j]==2) {
-						count_verticalal_o++;
-						if (count_verticalal_o>4) {
+					if (palya[j,i]==2) {
+						count_vertical_o++;
+						if (count_vertical_o>4) {
 							there_is_a_winner=true;
 							break;
 						}
 					} else {
-						count_verticalal_o=0;
-					}
-					
+						count_vertical_o=0;
+					}					
 				}
 				count_vertical_x=0;
-				count_verticalal_o=0;
+				count_vertical_o=0;
 			}
-			
-			/*
+			//***********átló vizsgálat**********************		
+			int temp_row,temp_col;
+			int count_diagonal_x=0,count_diagonal_o=0;
 			for (int i = 0; i < sorSzam; i++) {
 				for (int j = 0; j < oszlopSzam; j++) {
-					if (palya[i,j]==1) {
-						count_vertical++;
-						if (count_vertical>4) {
+					//left diagonal
+					if (i>=j) {
+						temp_row=i-j;
+						temp_col=0;
+					} else {
+						temp_row=0;
+						temp_col=j-i;
+					}
+					while (temp_row!=sorSzam && temp_col!=oszlopSzam) {
+						if (palya[temp_row,temp_col]==1) {
+							count_diagonal_x++;
+							if (count_diagonal_x>4) {
 							there_is_a_winner=true;
 							break;
+							}
+						} else {
+							count_diagonal_x=0;
 						}
+						temp_row++;
+						temp_col++;				
+					}				
+					//***********right diagonal**************
+					count_diagonal_x=0;
+					if (i+j>=sorSzam) {
+						temp_row=sorSzam;
+						temp_col=i+j-sorSzam;
+					} else {
+						temp_row=0;
+						temp_col=i+j;
 					}
+					while (temp_row!=sorSzam && temp_col!=0) {
+						if (palya[temp_row,temp_col]==1) {
+							count_diagonal_x++;
+							if (count_diagonal_x>4) {
+							there_is_a_winner=true;
+							break;
+							}
+						} else {
+							count_diagonal_x=0;
+						}				
+						if (palya[temp_row,temp_col]==2) {
+							count_diagonal_o++;
+							if (count_diagonal_o>4) {
+							there_is_a_winner=true;
+							break;
+							}
+						} else {
+							count_diagonal_o=0;
+						}			
+						temp_row++;
+						temp_col--;				
+					}				
 				}
-				count_vertical=0;
 			}
-			
-			
-			//vertical checks
-			else if ((A1.Text==B1.Text)&&(B1.Text==C1.Text)&&(!A1.Enabled)) {
-				there_is_a_winner=true;
-			}
-			else if((A2.Text==B2.Text)&&(B2.Text==C2.Text)&&(!A2.Enabled)) {
-				there_is_a_winner=true;
-			}
-			else if((A3.Text==B3.Text)&&(B3.Text==C3.Text)&&(!A3.Enabled)) {
-				there_is_a_winner=true;
-			}
-			
-			//diagonal checks
-			if ((A1.Text==B2.Text)&&(B2.Text==C3.Text)&&(!A1.Enabled)) {
-				there_is_a_winner=true;
-			}
-			else if((A3.Text==B2.Text)&&(B2.Text==C1.Text)&&(!A3.Enabled)) {
-				there_is_a_winner=true;
-			}
-			*/
-			
 			if (there_is_a_winner) {
-				//disableButtons();
 				String winner="";
 				if (x_or_not) {
 					winner="O";
-					//o_win_count.Text=(Int32.Parse(o_win_count.Text)+1).ToString();
 				}else{
 					winner="X";
-					//x_win_count.Text=(Int32.Parse(x_win_count.Text)+1).ToString();
 				}
 				MessageBox.Show(winner+" nyert!","Hurrá!");
-			}else{
+			}else{	//Kell ez?, jó ez?, később átgondolni
 				if (turn_count==sorSzam*oszlopSzam) {
-					//draw_count.Text=(Int32.Parse(draw_count.Text)+1).ToString();
 					MessageBox.Show("Döntetlen!","Nesze!");
 				}
 			}
-			
 		}
 	}
 }
