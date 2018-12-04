abbf5b450f36b0ad89ec3961392b7244eb52e3b2
broknfist <41538231+broknfist@users.noreply.github.com>
2018-12-04 22:17:59 +0100

    az egyik átló még mindig rossz
    ...

--- Gamefield.cs
+++ Gamefield.cs
@@ -16,6 +16,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 
+
+namespace System.Windows.Controls
+{
+    public static class MyExt
+    {
+         public static void PerformClick(this Button btn)
+         {
+             btn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+         }
+    }
+}
 namespace gomoku
 {
 	/// <summary>
@@ -24,11 +35,11 @@ namespace gomoku
 	public class Gamefield
 	{
 		private Window1 mainwindow;
-		private int sorSzam;
-		private int oszlopSzam;
-		private bool x_or_not=true;
-		private int turn_count=0;
-		private int[,] palya;
+		protected int sorSzam;
+		protected int oszlopSzam;
+		protected bool x_or_not=true;
+		protected int turn_count=0;
+		protected int[,] palya;
 		public Gamefield(Window1 mainwindow,int sorszam,int oszlopszam)
 		{
 			this.mainwindow=mainwindow;
@@ -55,6 +66,7 @@ namespace gomoku
 					Button gridButton=new Button();
 					palya[i,j]=0;
 					gridButton.Content="";				
+					gridButton.Name="CellBtn";
 					gridButton.Width=40;
 					gridButton.Height=40;
 					gridButton.Click+=buttonClick;
@@ -63,7 +75,7 @@ namespace gomoku
 					Grid.SetColumn(gridButton,j);
 				}
 			}
-			mainwindow.GameField.Children.Add(buttongrid);
+			mainwindow.Board.Children.Add(buttongrid);
 		}
 		private void buttonClick(object sender,RoutedEventArgs e){
 			Button gridButton=(Button)sender;
@@ -73,15 +85,115 @@ namespace gomoku
 				gridButton.Content="X";
 				x_or_not=false;
 				turn_count++;
+				checkForWinner();
+				foreach (Grid gr in mainwindow.Board.Children) {
+					foreach (Button bt in gr.Children) {
+						if (bt.IsEnabled){
+							
+							bt.Content="O";
+							palya[Grid.GetRow(bt),Grid.GetColumn(bt)]=2;
+							x_or_not=true;
+							turn_count++;
+							//gridButton.PerformClick(); or bt.PerformClick, ha kétjátékos lenne;
+							bt.IsEnabled=false;
+							break;
+						}
+					}
+				}
+				checkForWinner();
 			}else{
-				gridButton.IsEnabled=false;
-				gridButton.Content="O";
-				palya[Grid.GetRow(gridButton),Grid.GetColumn(gridButton)]=2;
-				x_or_not=true;
-				turn_count++;
+				/*
+			   foreach(UIElement child in grid.Children)  
+				   {  
+				      if(Grid.GetRow(child) == row  
+				            &&  
+				         Grid.GetColumn(child) == column)  
+				      {  
+				         return child;  
+				      }  
+				   }  
+ 				*/
+ 				/*
+ 				 * Itt volt a második játékos lépése, megszűntettem, amíg a gép nem lép jól, ha az megvan, átszervezem az egészet
+ 				*/
+				//gridButton.IsEnabled=false;
 			}			
-			checkForWinner();
+			//checkForWinner();
+			/*
+			if (x_or_not) {
+				Button btnRand=new Button();
+				Random vsz=new Random();
+				Grid.SetColumn(mainwindow.Board,1);
+				Grid.SetRow(mainwindow.Board,1);
+				btnRand=gridButton;
+				btnRand.PerformClick();
+					Ez valami régi szemét...
+			}
+			*/
 			//implement performclick button, ha make_a_move() visszatér egy jó lépéssel!!!
+			//Button btnOK=null;
+			/*
+			try{
+			foreach (Button c in mainwindow.Board.Children) {
+				btnOK=(Button)c; //as Button;  //this tries to cast all controls as a button, if it fails, it returns as a null				
+				if (btnOK != null) {
+					if (btnOK.Content=="") {
+						btnOK.PerformClick();
+					}
+				}
+			}
+			} catch { }
+			//btnOK.PerformClick();
+			*/
+			//make_move(gridButton);
+			//make_move();
+		}
+		
+		private void make_move(){
+			
+			//1: win
+			//2: ha x-nek 3 van és kirakhat ötöt, megakadályozni
+			//3: ha o-nak 3 van és kirakhat ötöt, letenni a negyediket
+			//4: ha o-nak 2 van és kirakhat ötöt, letenni mellé a harmadikat
+			//5: ha o-nak 1 van és kirakhat ötöt, letenni mellé a másodikat
+			//6: random o-nak, lehetőleg x közelébe
+			//Button btnOK=gridButton;
+			//Button btnOK=new Button();
+			//Random vsz=new Random();
+			//do{
+			//	Grid.SetColumn(mainwindow.Board,vsz.Next(0,16));
+			//	Grid.SetRow(mainwindow.Board,vsz.Next(0,16));	
+			/*
+			move=look_for_win_or_block("O"); //look for win
+			if (move==null) {
+				move=look_for_win_or_block("X"); //look for block
+				if (move==null) {
+					move=look_for_corner();
+					if (move==null) {
+						move=look_for_open_space();
+					}
+				}
+			}
+			*/
+			/*
+			Instead of PerformClick() use RaiseEvent()
+			
+			private void button1_Click(object sender, EventArgs e)
+			{
+			    MessageBox.Show("yes");
+			}
+			private void Form1_Load(object sender, EventArgs e)
+			{
+			    RoutedEventArgs newEventArgs = new RoutedEventArgs(Button.ClickEvent);
+			    button1.RaiseEvent(newEventArgs);         
+			}
+			*/
+			//}while(btnOK.Content=="");
+			//btnOK.PerformClick();
+			//btnOK=gridButton;
+			//RoutedEventArgs newEventArgs = new RoutedEventArgs(Button.ClickEvent);
+			//btnOK.RaiseEvent(newEventArgs);
+			
 		}
 		private void checkForWinner(){
 			bool there_is_a_winner=false;	
@@ -106,8 +218,7 @@ namespace gomoku
 						}
 					} else {
 						count_horizontal_o=0;
-					}
-					
+					}				
 				}
 				count_horizontal_x=0;
 				count_horizontal_o=0;
@@ -138,7 +249,87 @@ namespace gomoku
 				count_vertical_x=0;
 				count_vertical_o=0;
 			}
-			//***********átló vizsgálat**********************		
+			//***********átló vizsgálat**********************		Hibás!!!!
+			int temp_row,temp_col;
+			int count_left_diagonal_x=0,count_left_diagonal_o=0;
+			for (int i = 0; i < sorSzam; i++) {
+				for (int j = 0; j < oszlopSzam; j++) {
+					if (i>j) {
+						temp_row=i-j;
+						temp_col=0;
+					} else {
+						temp_row=0;
+						temp_col=j-i;
+					}
+					while (temp_row!=sorSzam && temp_col!=oszlopSzam) {
+						if (palya[temp_row,temp_col]==1) {
+							count_left_diagonal_x++;
+							if (count_left_diagonal_x>4) {
+							there_is_a_winner=true;
+							break;
+							}
+						} else {
+							count_left_diagonal_x=0;
+						}
+						if (palya[temp_row,temp_col]==2) {
+							count_left_diagonal_o++;
+							if (count_left_diagonal_o>4) {
+							there_is_a_winner=true;
+							break;
+							}
+						} else {
+							count_left_diagonal_o=0;
+						}
+						temp_row++;
+						temp_col++;
+					}
+					count_left_diagonal_x=0;	//ez a sarkok miatt kell
+					count_left_diagonal_o=0;	//még át kell gondolni miért, de működik
+				}
+			}
+			
+			//másik átló  //már csak ez a szar!
+			
+			
+			int count_right_diagonal_x=0,count_right_diagonal_o=0;
+			for (int i = 0; i < sorSzam; i++) {
+				for (int j = 0; j < oszlopSzam; j++) {
+					if (i>j) {
+						temp_row=i-j;
+						temp_col=0;
+					} else {
+						temp_row=0;
+						temp_col=j-i;
+					}
+					while (temp_row!=sorSzam && temp_col!=0) {
+						if (palya[temp_row,temp_col]==1) {
+							count_right_diagonal_x++;
+							if (count_right_diagonal_x>4) {
+							there_is_a_winner=true;
+							break;
+							}
+						} else {
+							count_right_diagonal_x=0;
+						}
+						if (palya[temp_row,temp_col]==2) {
+							count_right_diagonal_o++;
+							if (count_right_diagonal_o>4) {
+							there_is_a_winner=true;
+							break;
+							}
+						} else {
+							count_right_diagonal_o=0;
+						}
+						temp_row++;
+						temp_col--;
+					}
+					//count_right_diagonal_x=0;	//ez a sarkok miatt kell
+					//count_right_diagonal_o=0;	//még át kell gondolni miért, de működik
+				}
+			}
+			
+			
+			/*
 			int temp_row,temp_col;
 			int count_diagonal_x=0,count_diagonal_o=0;
 			for (int i = 0; i < sorSzam; i++) {
@@ -171,11 +362,13 @@ namespace gomoku
 							count_diagonal_o=0;
 						}
 						temp_row++;
-						temp_col++;				
+						temp_col++;					
 					}				
 				}
 			}
-			//***********right diagonal**************
+			*/
+			//***********right diagonal**************			Hibás!!!!
+			/*
 			int count_right_diagonal_x=0,count_right_diagonal_o=0;
 			for (int k = 0; k < sorSzam+oszlopSzam-2; k++) {
 				for (int j = 0; j <= k;  j++) {
@@ -188,7 +381,7 @@ namespace gomoku
 							break;
 							}
 						} else {
-							count_right_diagonal_x=0;
+							
 						}
 						if (palya[i,j]==2) {
 							count_right_diagonal_o++;
@@ -200,8 +393,16 @@ namespace gomoku
 							count_right_diagonal_o=0;
 						}
 					}
+					count_right_diagonal_x=0;
+					count_right_diagonal_o=0;
 				}
 			}
+			*/
+			
+			
+			
+			
+			//győztes megnevezése
 			if (there_is_a_winner) {
 				String winner="";
 				if (x_or_not) {
